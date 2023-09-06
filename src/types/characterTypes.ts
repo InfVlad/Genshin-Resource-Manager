0b4474784ddc0281a5086935e4c0e0fbe00d7117
Vladimir Infante <infantevladimir@gmail.com>
2023-09-06 14:29:50 -0500

    Added types generation from scrape results

---
+++ src/types/characterTypes.ts
@@ -0,0 +1,9 @@
+import type { allCharactersData } from '../data/charactersData';
+
+type BaseData = (typeof allCharactersData)[number];
+
+export type CharacterName = BaseData['name'];
+export type WeaponType = BaseData['weapon'];
+export type ElementType = BaseData['element'];
+export type RarityType = BaseData['rarity'];
+export type LocalSpecialityType = BaseData['localSpecialty'];
