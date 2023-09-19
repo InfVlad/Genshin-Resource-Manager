b8baec874d28003c38b6bb5ffc7b36cf5c649eda
Vladimir Infante <infantevladimir@gmail.com>
2023-09-18 21:14:36 -0500

    added colors to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
+import plugin from 'tailwindcss/plugin';
 
 const config: Config = {
   content: [
@@ -10,11 +11,35 @@ const config: Config = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'golden-st':
+          'linear-gradient(160deg, rgba(105, 84, 83, 0.565) 0%, rgba(161, 112, 78, 0.565) 39%, rgba(228, 171, 82, 0.565) 100%)',
+        'purple-st':
+          'linear-gradient(160deg, rgba(89, 84, 130, 0.565) 0%, rgba(120, 102, 157, 0.565) 39%, rgba(183, 133, 201, 0.565) 100%)',
+        'green-st':
+          'linear-gradient(160deg, rgba(72, 87, 92, 0.565) 0%, rgba(72, 107, 103, 0.565) 39%, rgba(98, 152, 113, 0.565) 100%)',
+        'gray-st':
+          'linear-gradient(160deg, rgba(79, 88, 100, 0.565) 0%, rgba(95, 102, 115, 0.565) 39%, rgba(135, 147, 156, 0.565) 100%)',
+      },
+      colors: {
+        'gray-primary': '#CCC',
+        'gray-secondary': '#7C7C7C',
+        'yellow-primary': '#FFD780',
+        'white-primary': '#ECE5D8',
+      },
+      fontFamily: {
+        montserrat: ['var(--font-montserrat)'],
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [
+    plugin(function ({ addComponents }) {
+      addComponents({
+        '.shadow-button': {
+          boxShadow: 'inset 0 0 2px 0 rgba(0, 0, 0, 0.5), 0 0 7px 0 hsla(39, 34%, 89%, 0.7)',
+        },
+      });
+    }),
+  ],
+};
+export default config;
