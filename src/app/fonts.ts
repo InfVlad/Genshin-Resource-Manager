e0a92b21cd12257f6869e8a8ebe53323e32c0407
Vladimir Infante <infantevladimir@gmail.com>
2023-09-20 19:22:50 -0500

    Added font config

---
+++ src/app/fonts.ts
@@ -0,0 +1,8 @@
+import { Montserrat } from 'next/font/google';
+
+export const montserrat = Montserrat({
+  weight: ['400', '600', '800'],
+  display: 'swap',
+  variable: '--font-montserrat',
+  subsets: ['latin'],
+});
