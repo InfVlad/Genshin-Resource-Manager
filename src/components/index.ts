220665d2aebf67160026cf3afbbd2d8b8be32bed
Vladimir Infante <infantevladimir@gmail.com>
2023-10-06 20:19:31 -0500

    Temporal: main page is for showcasing the components

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { Text } from '@/components/ui/Text/Text';
+export { Button } from '@/components/ui/Button/Button';
+export { HeaderButton } from '@/components/HeaderButton';
+export { MiniCard } from '@/components/MiniCard';
+export { FilterButton } from '@/components/FilterButton';
